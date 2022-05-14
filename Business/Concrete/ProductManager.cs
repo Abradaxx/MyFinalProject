2fa6454fabc1ff9a84ebeeaba681daca0a001704
Abradaxx <onur.kerem.bzkrt@gmail.com>
2022-05-14 20:37:53 +0300

    14.05.2022 ders sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,11 +1,7 @@
 ﻿using Business.Abstract;
 using DataAccess.Abstract;
-using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
-using System;
 using System.Collections.Generic;
-
-using System.Text;
 
 namespace Business.Concrete
 {
@@ -22,6 +18,17 @@ namespace Business.Concrete
         {
             //iş kodları
             return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,15 +1,20 @@
 ﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using System;
 
 namespace ConsoleUI
 {
+    //SOLID
+    //O---> Open Closed Principle 
+    //InMemoryden EfProductDal a çektim 
+
     class Program
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            foreach (var item in productManager.GetAll())
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var item in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(item.ProductName);
             }
